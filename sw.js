2766ea3e37b0371f7a26d4624cf5c7c29ae0f16b
ange01-ctrl <d9tsz62hgr@privaterelay.appleid.com>
2025-10-18 19:35:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// SW v5.2
-const CACHE='straordinari-v5-2';
+// SW v5.3
+const CACHE='straordinari-v5-3';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
